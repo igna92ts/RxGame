6e91173ffd5248a9e13d2fd7cadfd47abeb12c1f
igna92ts <igna_torres92@hotmail.com>
2017-06-30 02:55:21 -0300

    even more

---
+++ Assets/scripts/game/IlumEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IlumEntity : MonoBehaviour {
+
+	private float start;
+	private float end;
+	private float t = 0.0f;
+
+	public Light light;
+
+	void Start() {
+
+		start = light.intensity;
+ 
+		end = light.intensity/5;
+	}
+
+	void Update () {
+			t += Time.deltaTime;
+			if (light.intensity <= end) {
+				light.intensity = Mathf.Lerp (start, end, t / 2);
+			} 
+			// if (light.intensity >= start){
+			// 	light.intensity = Mathf.InverseLerp (start, end, t / 2);
+			// }
+			
+	}
+}
---
+++ Assets/scripts/game/MapMaker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMaker : MonoBehaviour {
+
+	private List<GameObject> tiles = new List<GameObject>();
+	void Start() {
+		var screenHeight = Screen.height;
+		var screenWidth = Screen.width;
+
+		var simpleTilePrefab = Instantiate(Resources.Load("enemy")) as GameObject;
+		RectTransform rt = (RectTransform)simpleTilePrefab.transform;
+ 
+		float width = rt.rect.width;
+		float height = rt.rect.height;
+		var yCount = (screenHeight / height) + 2;
+		var xCount = (screenWidth / width) + 2;
+
+		Debug.Log(xCount);
+		Debug.Log(yCount);
+	}
+}
